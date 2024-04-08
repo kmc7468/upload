116e37ace948b5fb5b13a9769852c5124fd63d33
static <kmc7468@naver.com>
2024-04-08 18:30:39 +0900

    logging 모듈을 이용하여 텍스트 파일 및 stdout에 로그를 기록하도록 개선

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from fastapi import FastAPI, Path, Request, Response
 from fastapi.responses import FileResponse, PlainTextResponse
 from fastapi_utilities import repeat_at
 from hashlib import sha256
+import logging
 from magic import Magic
 from os import environ, listdir, path, remove
 import random
@@ -41,19 +43,22 @@ ID_LENGTH = get_integer_env("ID_LENGTH", 6)
 ID_REGEX = f"^[a-zA-Z0-9]{{{ID_LENGTH}}}$"
 MAX_FILE_SIZE = get_integer_env("MAX_FILE_SIZE", 1073741824) # Default: 1 GiB
 FILE_EXPIRES = get_integer_env("FILE_EXPIRES", 3600) # Default: 1 hour
+TRUST_PROXY = get_integer_env("TRUST_PROXY", 0, 0) # Default: 0
 
 DIRECTORY = get_directory_env("DIRECTORY")
-#LOG_DIRECTORY = get_directory_env("LOG_DIRECTORY")
-
-print(f"PORT={PORT}")
-print(f"ID_LENGTH={ID_LENGTH}")
-print(f"MAX_FILE_SIZE={MAX_FILE_SIZE}")
-print(f"FILE_EXPIRES={FILE_EXPIRES}")
-print(f"DIRECTORY=\"{DIRECTORY}\"")
-#print(f"LOG_DIRECTORY=\"{LOG_DIRECTORY}\"")
+LOG_DIRECTORY = get_directory_env("LOG_DIRECTORY")
 
 app = FastAPI()
+logger = logging.getLogger(__file__)
 mime = Magic(mime=True)
+
+@app.get("/")
+def index():
+  try:
+    return FileResponse("static/index.html", media_type="text/html")
+  except Exception as e:
+    logger.error(e)
+    return Response(status_code=500)
 
 def generate_random_filename(length: int):
   chars = string.ascii_letters + string.digits
@@ -66,13 +71,14 @@ def generate_unique_filename():
     if not path.exists(path.join(DIRECTORY, filename)) and not path.exists(path.join(DIRECTORY, filename + ".d")):
       return filename
 
-@app.get("/")
-def index():
-  try:
-    return FileResponse("static/index.html", media_type="text/html")
-  except Exception as e:
-    print(e)
-    return Response(status_code=500)
+def get_client_ip(request: Request):
+  x_forwarded_for = request.headers.get("X-Forwarded-For")
+  if x_forwarded_for is not None:
+    return x_forwarded_for.split(",")[-(TRUST_PROXY + 1)].strip()
+  elif TRUST_PROXY > 0:
+    raise ValueError("X-Forwarded-For header must be set")
+  else:
+    return request.client.host
 
 async def upload_file_body(filename: str, request: Request, disposable: bool):
   try:
@@ -86,18 +92,20 @@ async def upload_file_body(filename: str, request: Request, disposable: bool):
     elif len(file) > MAX_FILE_SIZE:
       return Response(status_code=413)
 
+    hash = sha256(file).hexdigest()
+    client_ip = get_client_ip(request)
+
     target_filename = generate_unique_filename()
     target_filename_real = target_filename + (".d" if disposable else "")
     target_path = path.join(DIRECTORY, target_filename_real)
     with open(target_path, "wb") as target_file:
       target_file.write(file)
 
-    hash = sha256(file).hexdigest()
-    print(f"File \"{filename}\" uploaded as \"{target_filename_real}\" with hash \"{hash}\"")
+    logger.info(f"File \"{filename}\" uploaded as \"{target_filename_real}\" with hash \"{hash}\" by \"{client_ip}\"")
 
     return PlainTextResponse(f"https://{host}/{target_filename}/{quote(filename)}\n")
   except Exception as e:
-    print(e)
+    logger.error(e)
     return Response(status_code=500)
 
 @app.put("/{filename}")
@@ -116,33 +124,38 @@ def read_from_file(id: str, ext: str = ""):
   else:
     return None, None
 
-def make_file_response(filename: str, file: bytes):
+def make_file_response(id: str, filename: str, file: bytes, client_ip: str):
+  logger.info(f"File \"{id}\" downloaded by \"{client_ip}\"")
+
   disposition = f"attachment; filename={quote(filename)}" if filename is not None else "inline"
   return Response(content=file, media_type=mime.from_buffer(file), headers={"Content-Disposition": disposition}) 
 
-def download_file_body(id: str, filename: str | None):
+def download_file_body(id: str, filename: str | None, request: Request):
   try:
+    client_ip = get_client_ip(request)
+    make_200_response = lambda file, ext = "": make_file_response(id + ext, filename, file, client_ip)
+
     file, _ = read_from_file(id)
     if file is not None:
-      return make_file_response(filename, file)
+      return make_200_response(file)
 
     file, target_path = read_from_file(id, ".d")
     if file is not None:
       remove(target_path)
-      return make_file_response(filename, file)
+      return make_200_response(file, ".d")
 
     return Response(status_code=404)
   except Exception as e:
-    print(e)
+    logger.error(e)
     return Response(status_code=500)
 
 @app.get("/{id}")
-def download_file(id: str = Path(..., pattern=ID_REGEX)):
-  return download_file_body(id, None)
+def download_file(id: str = Path(..., pattern=ID_REGEX), request: Request = None):
+  return download_file_body(id, None, request)
 
 @app.get("/{id}/{filename}")
-def download_file_as(id: str = Path(..., pattern=ID_REGEX), filename: str = Path(...)):
-  return download_file_body(id, filename)
+def download_file_as(id: str = Path(..., pattern=ID_REGEX), filename: str = Path(...), request: Request = None):
+  return download_file_body(id, filename, request)
 
 @app.on_event("startup")
 @repeat_at(cron="* * * * *")
@@ -153,7 +166,23 @@ def cleanup():
       if path.isfile(target_path) and path.getmtime(target_path) + FILE_EXPIRES < time():
         remove(target_path)
   except Exception as e:
-    print(e)
+    logger.error(e)
 
 if __name__ == "__main__":
+  logger.level = logging.DEBUG
+  logger.handlers = [
+    logging.FileHandler(path.join(LOG_DIRECTORY, datetime.now().strftime("%Y%m%d-%H%M%S") + ".log")),
+    logging.StreamHandler()
+  ]
+  for handler in logger.handlers:
+    handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S"))
+
+  logger.debug(f"PORT={PORT}")
+  logger.debug(f"ID_LENGTH={ID_LENGTH}")
+  logger.debug(f"MAX_FILE_SIZE={MAX_FILE_SIZE}")
+  logger.debug(f"FILE_EXPIRES={FILE_EXPIRES}")
+  logger.debug(f"TRUST_PROXY={TRUST_PROXY}")
+  logger.debug(f"DIRECTORY=\"{DIRECTORY}\"")
+  logger.debug(f"LOG_DIRECTORY=\"{LOG_DIRECTORY}\"")
+
   uvicorn.run(app, host="0.0.0.0", port=PORT)
