cae8b8e2387d87be965c87c013314facd5868fc3
static <kmc7468@naver.com>
2024-06-22 22:53:23 +0900

    Dockerfile 및 docker-compose.yml 파일 마이그레이션

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,6 @@ export const ONE_MEBI = 1024 * ONE_KIBI;
 export const ONE_GIBI = 1024 * ONE_MEBI;
 
 export const MAX_FILE_SIZE =
-  parseInt(env.PUBLIC_MAX_FILE_SIZE ?? "1073741824", 10); // Default: 1 GiB
+  parseInt(env.PUBLIC_MAX_FILE_SIZE || "1073741824", 10); // Default: 1 GiB
 export const MAX_CONVERTIBLE_IMAGE_SIZE =
-  parseInt(env.PUBLIC_MAX_CONVERTIBLE_IMAGE_SIZE ?? "31457280", 10); // Default: 30 MiB
+  parseInt(env.PUBLIC_MAX_CONVERTIBLE_IMAGE_SIZE || "31457280", 10); // Default: 30 MiB
